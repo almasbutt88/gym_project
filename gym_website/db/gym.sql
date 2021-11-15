387c7a2fedd42f8bc266a74b97f44e1b47fe5be5
Almas <m_a_b19@hotmail.com>
2021-11-15 13:57:45 +0000

    database set up, working on routes next

--- gym_website/db/gym.sql
+++ gym_website/db/gym.sql
@@ -5,16 +5,27 @@ CREATE TABLE members(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     age INT,
-    fav_class VARCHAR (255)
+    booked_class VARCHAR (255)
 );
 
 CREATE TABLE gym_classes(
     id SERIAL PRIMARY KEY,
-    mwembers VARCHAR(255),
+    name VARCHAR(255),
+    members INT,
     capacity INT
 );
 
-INSERT INTO members (name, age, fav_class) VALUES ('Joe', 40, 'spin');
-INSERT INTO members (name, age, fav_class) VALUES ('Mary', 55, 'zoomba');
-INSERT INTO members (name, age, fav_class) VALUES ('Lucy', 24, 'pilates');
-INSERT INTO members (name, age, fav_class) VALUES ('Chris', 22, 'body combat');
+INSERT INTO members (name, age, booked_class) VALUES ('Joe', 40, 'Spin');
+INSERT INTO members (name, age, booked_class) VALUES ('Mary', 55, 'Zoomba');
+INSERT INTO members (name, age, booked_class) VALUES ('Lucy', 24, 'Spin');
+INSERT INTO members (name, age, booked_class) VALUES ('Chris', 17, 'Spin');
+INSERT INTO members (name, age, booked_class) VALUES ('Fraser', 40, 'Spin');
+INSERT INTO members (name, age, booked_class) VALUES ('Lee', 55, 'Zoomba');
+INSERT INTO members (name, age, booked_class) VALUES ('Sean', 24, 'Pilates');
+INSERT INTO members (name, age, booked_class) VALUES ('Mike', 17, 'Body Combat');
+
+
+INSERT INTO gym_classes (name, members, capacity) VALUES ('Spin', 4, 5);
+INSERT INTO gym_classes (name, members, capacity) VALUES ('Zoomba', 2, 5);
+INSERT INTO gym_classes (name, members, capacity) VALUES ('Pilates', 1, 5);
+INSERT INTO gym_classes (name, members, capacity) VALUES ('Body Combat', 1, 5);
